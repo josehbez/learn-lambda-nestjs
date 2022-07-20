250e0dcb97e94e58ccefb5dcf5f731265ddb7e26
Jose Hbez <dev@josehbez.com>
2022-07-19 20:56:48 -0500

    Added: mar 19 jul 2022 20:56:48 CDT

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-
+/*
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
@@ -21,4 +21,4 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
-});
+});*/
